6d77cbe67b01df325fecae82fb19967d520d964b
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-11-16 14:29:44 +0100

    Added actual bug list

--- utilityMonteCarloSimulation.R
+++ utilityMonteCarloSimulation.R
@@ -20,20 +20,24 @@ source("C://Users//chris//OneDrive//Documentos//GitHub//ML_VotingAggregation//lo
 answers_df <- loadAnswers("answerList_data.csv");
 
 #Initialize variables
-accumOutcomes<- list();
-initialize(); #confusionMatrix
+accumStatistics<- list();
+actualBugs <- c(1,4,10,14,20,23,30,32,55,56,57,58,59,72,73,77,84,92,95,97,102,104,115,119,123);
 
-#start with minimal answers (5 per question)
+#start with one answer
 questionID_f <- data.frame(unique(answers_df$Question.ID));
 colnames(questionID_f)<- c("id");
-sampled_dataf<-sampleWithReplacement(questionList=questionID_f,answers_df = answers_df, sampleSize = 5);
+
+#Sample with replacement fits the assumptions of infinite population, unknown distribution, 
+#and equal probability of any programmer to provide an answer
+sampled_dataf<-sampleWithReplacement(questionList=questionID_f,answers_df = answers_df, sampleSize = 1);
 
 #for(i in 1:4){
   #### compute precision recall accuracy sensibility sensitivity
   answersByQuestions_df <- runFromSample(sampled_dataf);
-  bugCoveringPredictedList <- selectPredictedBugs(rankedSelection = answersByQuestions_df);##BUGGY STILL
-  outcomesf<- computeOutcomes(bugCoveringPredictedList);
-  accumOutcomes <- rbind(accumOutcomes,outcomesf);
+  #Select only the question with ranking position equals 1 or 2
+  predictedBugs <- answersByQuestions_df[answersByQuestions_df$rankingVote==1 | answersByQuestions_df$rankingVote==2,];
+  statistics_f<- computeStatistics(predictedBugs,actualBugs); #change name of computeOutcomes to computeStat
+  accumStatistics <- rbind(accumStatistics,statistics_f);
   
   #compute utility
   utility_table<-confidence_utility(df=sampled_dataf);
