cf4602456f15b5e153c6b463070deedaf5362a53
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-11-16 14:28:57 +0100

    New sampling

---
+++ sampleFunctions.R
@@ -0,0 +1,17 @@
+#Sampling approaches
+
+#sample N answers with replacement for each question
+sampleWithReplacement<- function(questionList, answers_df, sampleSize){
+  sampled_dataf<-data.frame();
+  for(id in questionList$id){
+    questionSet<-answers_df[answers_df$Question.ID==id,]
+    sampled_df<- sample_n(questionSet, sampleSize)
+    sampled_dataf<-rbind(sampled_dataf,sampled_df);
+  }
+  return(sampled_dataf);
+}
+
+
+#sample N answers WITHOUT replacement for each question
+sampleWithoutReplacement<- function(questionList, answers_df, sampleSize){
+}
