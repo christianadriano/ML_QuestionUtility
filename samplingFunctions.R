89132f6ecb5ac97241c86606dd89ed1abaade35f
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-11-16 14:29:27 +0100

    Explicit call to library function

--- samplingFunctions.R
+++ samplingFunctions.R
@@ -1,11 +1,13 @@
 #Sampling approaches
 
+library(dplyr);
+
 #sample N answers with replacement for each question
 sampleWithReplacement<- function(questionList, answers_df, sampleSize){
   sampled_dataf<-data.frame();
   for(id in questionList$id){
     questionSet<-answers_df[answers_df$Question.ID==id,]
-    sampled_df<- sample_n(questionSet, sampleSize)
+    sampled_df<- dplyr::sample_n(questionSet, sampleSize)
     sampled_dataf<-rbind(sampled_dataf,sampled_df);
   }
   return(sampled_dataf);
