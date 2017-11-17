def7fa834d428dcbf75d450c9a7521287e5404c0
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-11-17 10:02:42 +0100

    Included scrambling

--- samplingFunctions.R
+++ samplingFunctions.R
@@ -4,6 +4,12 @@ library(dplyr);
 
 #sample N answers with replacement for each question
 sampleWithReplacement<- function(questionList, population, sampleSize){
+  
+  #Scramble the dataset before sampling.
+  set.seed(8850);
+  g<- runif((nrow(population))); #generates a random distribution
+  population <- population[order(g),];
+  
   sample_final<-data.frame();
   for(id in questionList$Question.ID){
     questionSet<-population[population$Question.ID==id,]
