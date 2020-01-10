512ca1e0605994211c015dc6859542b5e71b9b70
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-01-10 19:24:54 +0100

    Update probabilityYesAnswers_BayesianUpdate.R

--- probabilityYesAnswers_BayesianUpdate.R
+++ probabilityYesAnswers_BayesianUpdate.R
@@ -21,3 +21,38 @@ sim_bayes<-function(p=0.5,N=10,y_lim=15)
 }
 
 sim_bayes(p=0.6,N=90)
+
+# True distribution
+p <- 0.3
+n <- 10
+
+#add new answer
+num_yes <- 1 #
+#recompute probability (#Yes/Total Answers)
+
+"
+###Sampling of answers
+Sample one answer at a time from the question, then update the likelihood.
+Sample with replacement now, so I can use binomial, because I am only
+interested in Yes's (success) and not-Yes (failure). Not-Yes are No and I don't know
+answers.
+
+In the future I will sample without replacement, hence will have to use hypergeometric distribution"
+
+# Prior on p (n assumed known), discretized
+probability_of_yes <- seq(0.01,0.99,0.001)
+prior <- dbeta(probability_of_yes,1,1)
+prior <- 1000 * pr / sum(prior)  # Have to normalize given discreteness
+plot(prior~probability_of_yes, col=1, ylim=c(0, 14), type="l")
+
+# Run for 20 samples
+for (i in 1:20) {
+  prior <- 
+  p_likelihood <- 
+  x <- rbinom(1, n, p) 
+  ps <- dbinom(x, n, p_values) * pr
+  ps <- 1000 * ps / sum(ps)
+  lines(ps~p_values, col=(i+1))
+  
+  pr = ps
+}
