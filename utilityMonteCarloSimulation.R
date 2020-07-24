214a5d80fa5dae6dbfa6891c3b15e7ff1c43a93c
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-07-24 23:37:01 +0200

    Update utilityMonteCarloSimulation.R

--- utilityMonteCarloSimulation.R
+++ utilityMonteCarloSimulation.R
@@ -7,7 +7,6 @@ library(ggplot2)
 
 #TODO
 #Print results by Java method
-#Compute the probability of YES (two alternatives - MarkovChain and Bayesian Update)
 
 ######################################################################################################
 
@@ -25,7 +24,7 @@ source(paste0(path,"ML_VotingAggregation//loadAllAnswers.R"));
 answerPopulation_df <- loadAnswers("answerList_data.csv");
 
 #Initialize variables
-accumStatistics<- list(precision=0, recall=0, sensitivity=0, accuracy=0, answers=0);
+accumStatistics <- data.frame(list(precision=0, recall=0, sensitivity=0, accuracy=0, answers=0,mean_precision=0,mean_recall=0));
 actualBugs <- c(1,4,10,14,20,23,30,32,55,56,57,58,59,72,73,77,84,92,95,97,102,104,115,119,123);
 topQuestions <-list();
 
@@ -36,6 +35,17 @@ questionsToSelect <- 3;
 answerSamplingStep <- 1;
 #How many of the top ranking should be considered to predict bugs
 rankingTop <- 2;
+#Determines if utility will be multiplied by the probability of YES
+is_expected_utility=TRUE
+#Type of utility function
+utility_function="confidence"
+#How many times repeats the sampling
+Horizon = 20
+#Worker Score
+score=3
+
+answerPopulation_df <- answerPopulation_df[answerPopulation_df$Worker.score==score,]
+
 
 #start with one answer
 questionID_f <- data.frame(unique(answerPopulation_df$Question.ID));
@@ -47,18 +57,29 @@ sampled_dataf<-sampleWithReplacement(questionList=questionID_f,
                                      population = answerPopulation_df, 
                                      sampleSize = answerSamplingStep);
 
-for(i in 1:10){
+for(i in 1:Horizon){
   #### compute precision recall accuracy sensitivity
   answersByQuestions_df <- runFromSample(sampled_dataf);
   #Select only the question with ranking position equal of higher than rankingTop (see setup above)
   predictedBugs <- answersByQuestions_df[answersByQuestions_df$rankingVote<=rankingTop,];
   statistics_f<- computeStatistics(predictedBugs,actualBugs); #change name of computeOutcomes to computeStat
   statistics_f$answers <- dim(sampled_dataf)[1];
+  statistics_f$mean_precision <- compute_incremental_mean(n=dim(accumStatistics)[1],
+                                                          original_mean=mean(accumStatistics$precision),
+                                                          new_datapoint=statistics_f$precision)
+  
+  statistics_f$mean_recall <- compute_incremental_mean(n=dim(accumStatistics)[1],
+                                                      original_mean=mean(accumStatistics$recall),
+                                                      new_datapoint=statistics_f$recall)
   accumStatistics <- rbind(accumStatistics,statistics_f);
   
   #compute utility (or expected utility depending on the flag)
-  utility_table<-difficulty_utility(df=sampled_dataf,is_expected_utility=TRUE);
-
+  if(utility_function=="difficulty"){
+    utility_table<-difficulty_utility(df=sampled_dataf,is_expected_utility=is_expected_utility);
+  }
+  else{
+    utility_table<-confidence_utility(df=sampled_dataf,is_expected_utility=is_expected_utility);
+  }
   #select top questions from each JavaMethod
   topQuestions <- selectTopQuestionsByJavaMethod(utility_table,
                                                  sampled_dataf,
@@ -73,68 +94,42 @@ for(i in 1:10){
   sampled_dataf <- rbind(sampled_dataf,sampledAnswers_topQuestions);
 }
 
-plotOutcomes(utilityType="difficulty",U=questionsToSelect,A=answerSamplingStep,R=rankingTop);
+plotOutcomes(utilityFunction=utility_function,is_expected_utility=is_expected_utility,
+             U=questionsToSelect,A=answerSamplingStep,R=rankingTop,H=Horizon,score=score
+             );
 
 #########################################
 #### PLOT RESULTS #######################
-plotOutcomes<- function(utilityType,U,R,A){
+plotOutcomes<- function(utilityFunction,is_expected_utility,U,R,A,H,score=score){
   
-  hyperparam <- paste("U",U,"A",A,"R",R);
-  name <- paste(utilityType,i,hyperparam,"line-plot.jpg");
+  if(is_expected_utility)
+    utilityType <- paste(utilityFunction,"expected utility")
+  else
+    utilityType <- paste(utilityFunction,"utility")
+  
+  hyperparam <- paste0(utilityType,",","U",U,",","A",A,",","R",R,",","H",H,",","S",score);
+  name <- paste(hyperparam,"line-plot.jpg");
   
   ggplot(accumStatistics,aes(answers)) + 
-    geom_line(aes(y=100*precision, colour="precision")) + 
-    geom_line(aes(y=100*recall, colour="recall")) + 
-    geom_point(aes(x=answers,y=100*precision), shape=1) +
-    geom_point(aes(x=answers,y=100*recall), shape=1) + 
+    geom_line(aes(y=100*mean_precision, colour="precision")) + 
+    geom_line(aes(y=100*mean_recall, colour="recall")) + 
+    geom_point(aes(x=answers,y=100*mean_precision), shape=1) +
+    geom_point(aes(x=answers,y=100*mean_recall), shape=1) + 
     labs(y="%",x="answers") + 
-    labs(title =paste("precision, recall by answers",hyperparam));
+    labs(title =paste0("Mean Precision, Recall: ",hyperparam));
   
   
   ggsave(filename=name,last_plot(),device = "jpeg", 
-         path=paste0(path,"ML_QuestionUtility//utilityPlots//"));
+         path=paste0(path,"ML_QuestionUtility//expected_utility_plots//"));
   
   ggplot(sampled_dataf,aes(sampled_dataf$Question.ID)) + 
     geom_histogram(binwidth = 1) +
-    labs(title=paste("Answer per Question",hyperparam)) +
+    labs(title=paste("Answers/Question: ",hyperparam)) +
     labs(x="Question.ID", y="Total answers") + 
     xlim(c(1,130));
   
-  name <- paste(utilityType,i,hyperparam,"hist-plot.jpg");
+  name <- paste(hyperparam,"hist-plot.jpg");
   ggsave(filename=name,last_plot(),device = "jpeg",
-         path=paste0(path,"ML_QuestionUtility//utilityPlots//"));
+         path=paste0(path,"ML_QuestionUtility//expected_utility_plots//"));
 }
   
-#######################################
-#Select top questions within each JavaMethod
-selectTopQuestionsByJavaMethod <- function (utility_Table,
-                                            sampled_dataf,
-                                            questionsToSelect){
-  topQuestions <- list();
-  javaMethodList<- unique(sampled_dataf$FailingMethod);
-  
-  for(javaMethod in javaMethodList){
-    javaMethod_df <- sampled_dataf[sampled_dataf$FailingMethod==javaMethod,];
-    questionList <- unique(javaMethod_df$Question.ID);
-    
-    utilitySelection <- utility_Table[questionList,];
-    
-    utilityValues <- unique(utilitySelection$utility);
-    sizeL<-length(utilityValues);
-    if(sizeL>=questionsToSelect){
-      #take only the top values
-      index <- questionsToSelect;    
-    }
-    else{
-      index <- length(utilityValues); #fewer questions than the questionsToSelect step
-    }
-    
-    utilityValues<-utilityValues[order(utilityValues,decreasing = TRUE)];
-    utilityValues <- utilityValues[1:index]; 
-    
-    #take the top questions by utility and sample another set answers from them
-    topQuestions <- rbind(topQuestions, 
-                          data.frame(utilitySelection[utilitySelection$utility>=min(utilityValues),]));
-  }
-  return(topQuestions);
-}
